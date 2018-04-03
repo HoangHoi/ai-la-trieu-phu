d221f33d9db59711c81dcbe57ff6d6a5a6ba8217
Hoang Hoi <hoanghoi1310@gmail.com>
2018-04-03 15:09:05 +0700

    add_question_content

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,7 +15,7 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-4">
-                <div class="d-flex flex-column-reverse question">
+                <div class="d-flex flex-column-reverse question-list">
                     @for($i = 1; $i < 4; $i++)
                         @for($j = ($i - 1)*5 + 1; $j <= $i*5; $j++)
                             <div class="d-flex align-items-center question-item" id="question_{!! $j !!}">
@@ -29,39 +29,42 @@
                 </div>
             </div>
             <div class="col-md-8">
-                <div class="d-flex justify-content-start">
-                    <div class="" style="width: 136px">
-                        <div class="countdown">
-                            <div class="countdown-container" id="countdown"></div>
-                        </div>
-                        <script type="text/template" id="countdown-template">
-                            <div class="time <%= label %>">
-                                <span class="count curr top"><%= curr %></span>
-                                <span class="count next top"><%= next %></span>
-                                <span class="count next bottom"><%= next %></span>
-                                <span class="count curr bottom"><%= curr %></span>
+                <div class="content">
+                    <div class="d-flex justify-content-start">
+                        <div class="" style="width: 136px">
+                            <div class="countdown">
+                                <div class="countdown-container" id="countdown"></div>
                             </div>
-                        </script>
+                            <script type="text/template" id="countdown-template">
+                                <div class="time <%= label %>">
+                                    <span class="count curr top"><%= curr %></span>
+                                    <span class="count next top"><%= next %></span>
+                                    <span class="count next bottom"><%= next %></span>
+                                    <span class="count curr bottom"><%= curr %></span>
+                                </div>
+                            </script>
+                        </div>
                     </div>
-                </div>
-                <div>
-                    Noi dung cau hoi Noi dung cau hoi Noi dung cau hoi Noi dung cau hoi Noi dung cau hoi Noi dung cau hoi
-                </div>
-                <div>
-                    <ul>
-                            <Li>
-                                A. Dap an 1
-                            </Li>
-                            <Li>
-                                B. Dap an 2
-                            </Li>
-                            <Li>
-                                C. Dap an 3
-                            </Li>
-                            <Li>
-                                D. Dap an 4
-                            </Li>
-                    </ul>
+
+                    <div class="question-container"
+                        <div class="question-content">
+                            Noi dung cau hoi Noi dung cau hoi Noi dung cau hoi Noi dung cau hoi Noi dung cau hoi Noi dung cau hoi
+                        </div>
+                        <div class="d-flex flex-wrap question-answer">
+                            <div class="question-answer-item">
+                                <span>A. Đáp án 1</span>
+                            </div>
+                            <div class="question-answer-item">
+                                <span>B. Đáp án 2</span>
+                            </div>
+                            <div class="question-answer-item">
+                                <span>C. Đáp án 3</span>
+                            </div>
+                            <div class="question-answer-item">
+                                <span>D. Đáp án 4</span>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
